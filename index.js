c4fd5dd987ab8341a403a46b0b147822a78a9908
Ryan Driscoll <ryan@Ryans-MacBook-Pro.local>
2017-10-05 20:49:33 -0500

    got my stop keywords

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const ApiKey = process.env.cta_api_key || require('./secret');
 const ctaApiPrefix = 'http://www.ctabustracker.com/bustime/api/v2/';
 const format = '&format=json';
 const damen = require('./50_damen');
+const allRoutes = require('./allRoutes');
 
 function writeToFile(data, file) {
   const json = JSON.stringify(data, null, 2)
@@ -11,35 +12,41 @@ function writeToFile(data, file) {
     err ? console.error(err) : console.log('file written successfully');
   })
 }
+function writeToFileCsv(data, file) {
+  const csv = data.map(obj => '"' + obj.value + '"').join('\n')
+  fs.writeFile(file, csv, 'utf8', (err) => {
+    err ? console.error(err) : console.log('file written successfully');
+  })
+}
 
 function getRoutes() {
   const routeObj = {};
   return axios.get(`${ctaApiPrefix}getroutes?key=${ApiKey}${format}`)
-  .then(res => res.data['bustime-response'].routes)
-  .then(routes => {
-    routes.forEach(route => {
-      routeObj[route.rt] = {};
-    });
-    return routeObj;
-  })
-  .catch(err => console.error(err));
+    .then(res => res.data['bustime-response'].routes)
+    .then(routes => {
+      routes.forEach(route => {
+        routeObj[route.rt] = {};
+      });
+      return routeObj;
+    })
+    .catch(err => console.error(err));
 };
 
 function getDirections(routes) {
   const routesWithDir = [];
   return Promise.all(Object.keys(routes).map(route => {
     return axios.get(`${ctaApiPrefix}getdirections?key=${ApiKey}&rt=${route}${format}`)
-    .then(res => res.data['bustime-response'].directions)
-    .then(directions => {
-      directions.forEach(direction => {
-        routes[route][direction.dir] = {}
+      .then(res => res.data['bustime-response'].directions)
+      .then(directions => {
+        directions.forEach(direction => {
+          routes[route][direction.dir] = {}
+        })
       })
-    })
-    .catch(error => console.error(error))
+      .catch(error => console.error(error))
   }))
-  .then(() => {
-    return routes;
-  })
+    .then(() => {
+      return routes;
+    })
 }
 
 function getStops(routes) {
@@ -50,26 +57,120 @@ function getStops(routes) {
       let path = `${ctaApiPrefix}getstops?key=${ApiKey}&rt=${route}&dir=${direction}${format}`;
       promiseArray.push(
         axios.get(path)
-        .then(res => res.data['bustime-response'].stops)
-        .then(stops => {
-          stops.forEach(stop => {
-            routes[route][direction][stop.stpid] = stop.stpnm;
+          .then(res => res.data['bustime-response'].stops)
+          .then(stops => {
+            stops.forEach(stop => {
+              routes[route][direction][stop.stpid] = stop.stpnm;
+            })
           })
-        })
-        .catch(error => console.log(error))
+          .catch(error => console.log(error))
       )
     })
   })
   return Promise.all(promiseArray)
-  .then(() => {
-    writeToFile(routes, 'allRoutes.json')
-  })
-  .catch(error => console.log(error))
+    .then(() => {
+      writeToFile(routes, 'allRoutes.json')
+    })
+    .catch(error => console.log(error))
 }
 
-getRoutes()
-.then(routes => getDirections(routes))
-.then(routesWithDir => getStops(routesWithDir))
+String.prototype.replaceAll = function (search, replacement) {
+  const target = this;
+  return target.split(search).join(replacement);
+};
+
+
+function mapKeywords(allRoutes) {
+  const routesWithKeywords = {};
+  const memo = {};
+  const keywordValues = [];
+  for (let route in allRoutes) {
+    if (allRoutes.hasOwnProperty(route)) {
+      routesWithKeywords[route] = {};
+      for (let direction in allRoutes[route]) {
+        if (allRoutes[route].hasOwnProperty(direction)) {
+          routesWithKeywords[route][direction] = {};
+          for (let stopId in allRoutes[route][direction]) {
+            if (allRoutes[route][direction].hasOwnProperty(stopId)) {
+              console.log(allRoutes[route][direction][stopId])
+              let stopName = allRoutes[route][direction][stopId];
+              let keywords = stopName
+                .replaceAll('&', '')
+                .replaceAll('& ', '')
+                .replaceAll('/', ' ')
+                .replaceAll('HS', 'Highschool')
+                .replaceAll('S ', 'South ')
+                .replaceAll('S. ', 'South ')
+                .replaceAll('S)', 'South ')
+                .replaceAll('N ', 'North ')
+                .replaceAll('N. ', 'North ')
+                .replaceAll('N)', 'North ')
+                .replaceAll('E ', 'East ')
+                .replaceAll('E. ', 'East ')
+                .replaceAll('E)', 'East ')
+                .replaceAll('W ', 'West ')
+                .replaceAll('w ', 'West ')
+                .replaceAll('W. ', 'West ')
+                .replaceAll('W)', 'West ')
+                .replaceAll('Bldg.', 'Building')
+                .replaceAll('Bldg', 'Building')
+                .replaceAll('Blvd', 'Boulevard')
+                .replaceAll('Dr ', 'Drive ')
+                .replaceAll('Ave', 'Avenue')
+                .replaceAll('Rd', 'Road')
+                .replaceAll('Hwy', 'Highway')
+                .replaceAll('Mt', 'Mount')
+                .replaceAll('SB', 'Southbound')
+                .replaceAll('Pres ', 'Presbyterian ')
+                .replaceAll('mid', '')
+                .replaceAll('St.', 'Saint')
+                .replaceAll('Prking Ent', 'Parking Entrance')
+                .replaceAll('ST', '')
+                .replaceAll('II', '')
+                .replaceAll('(', '')
+                .replaceAll(')', '')
+                .replaceAll('-', ' ')
+                .replaceAll('.', '')
+                .split(' ')
+                .filter(word => word !== '');
+              keywords.forEach(word => {
+                if (routesWithKeywords[route][direction][word]) {
+                  routesWithKeywords[route][direction][word].push(stopId);
+                } else {
+                  routesWithKeywords[route][direction][word] = [stopId]
+                }
+              })
+            }
+          }
+          for (let keyword in routesWithKeywords[route][direction]) {
+            if (routesWithKeywords[route][direction].hasOwnProperty(keyword)) {
+              if (routesWithKeywords[route][direction][keyword].length > 20) {
+                delete routesWithKeywords[route][direction][keyword];
+              }
+              if (routesWithKeywords[route][direction][keyword] && !memo[keyword]) {
+                memo[keyword] = 1;
+                keywordValues.push({
+                  value: keyword,
+                  synonyms: [keyword]
+                })
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  console.log('$$$$$$', keywordValues.length)
+  writeToFile(routesWithKeywords, 'routesWithKeywords.json')
+  writeToFile(keywordValues, 'keywordValues.json')
+  writeToFileCsv(keywordValues, 'keyValues.csv')
+}
+
+mapKeywords(allRoutes);
+
+// getRoutes()
+// .then(routes => getDirections(routes))
+// .then(routesWithDir => getStops(routesWithDir))
 
 // function getDamen() {
 //   const directions = ['Northbound', 'Southbound'];
