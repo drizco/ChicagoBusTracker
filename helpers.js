4cbd2fe5fbdd7069f57d014af7e2f58f5797a8ae
ryan <driscollrp@gmail.com>
2017-10-16 10:39:33 -0500

    testing with dummy data

--- helpers.js
+++ helpers.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 const fs = require('fs');
 const ApiKey = process.env.cta_api_key || require('./config/secret');
+
 const ctaApiPrefix = 'http://www.ctabustracker.com/bustime/api/v2/';
 const format = '&format=json';
 const damen = require('./data/50_damen');
@@ -8,16 +9,16 @@ const allRoutes = require('./data/allRoutes');
 const routeMap = require('./data/routesWithKeywords');
 
 function writeToFile(data, file) {
-  const json = JSON.stringify(data, null, 2)
-  fs.writeFile(file, json, 'utf8', (err) => {
+  const json = JSON.stringify(data, null, 2);
+  fs.writeFile(file, json, 'utf8', err => {
     err ? console.error(err) : console.log('file written successfully');
-  })
+  });
 }
 function writeToFileCsv(data, file) {
-  const csv = data.map(obj => '"' + obj.value + '"').join('\n')
-  fs.writeFile(file, csv, 'utf8', (err) => {
+  const csv = data.map(obj => `"${obj.value}"`).join('\n');
+  fs.writeFile(file, csv, 'utf8', err => {
     err ? console.error(err) : console.log('file written successfully');
-  })
+  });
 }
 
 function getRoutes() {
@@ -31,61 +32,56 @@ function getRoutes() {
       return routeObj;
     })
     .catch(err => console.error(err));
-};
+}
 
 function getDirections(routes) {
   const routesWithDir = [];
-  return Promise.all(Object.keys(routes).map(route => {
-    return axios.get(`${ctaApiPrefix}getdirections?key=${ApiKey}&rt=${route}${format}`)
-      .then(res => res.data['bustime-response'].directions)
-      .then(directions => {
-        directions.forEach(direction => {
-          routes[route][direction.dir] = {}
-        })
-      })
-      .catch(error => console.error(error))
-  }))
-    .then(() => {
-      return routes;
+  return Promise.all(Object.keys(routes).map(route => axios.get(`${ctaApiPrefix}getdirections?key=${ApiKey}&rt=${route}${format}`)
+    .then(res => res.data['bustime-response'].directions)
+    .then(directions => {
+      directions.forEach(direction => {
+        routes[route][direction.dir] = {};
+      });
     })
+    .catch(error => console.error(error))))
+    .then(() => routes);
 }
 
 function getStops(routes) {
   const promiseArray = [];
-  let stopsObj = {};
+  const stopsObj = {};
   Object.keys(routes).forEach(route => {
     Object.keys(routes[route]).forEach(direction => {
-      let path = `${ctaApiPrefix}getstops?key=${ApiKey}&rt=${route}&dir=${direction}${format}`;
+      const path = `${ctaApiPrefix}getstops?key=${ApiKey}&rt=${route}&dir=${direction}${format}`;
       promiseArray.push(
         axios.get(path)
           .then(res => res.data['bustime-response'].stops)
           .then(stops => {
             stops.forEach(stop => {
               stopsObj[stop.stpid] = stop.stpnm.replaceFunc();
-              console.log(stop.stpnm.replaceFunc())
-            })
+              console.log(stop.stpnm.replaceFunc());
+            });
             return null;
           })
           .catch(error => console.log(error))
-      )
-    })
-  })
+      );
+    });
+  });
   return Promise.all(promiseArray)
     .then(() => {
-      writeToFile(stopsObj, 'stops.json')
+      writeToFile(stopsObj, 'stops.json');
     })
-    .catch(error => console.log(error))
+    .catch(error => console.log(error));
 }
 
 function getArrivals(stopID, route) {
   return axios.get(`${ctaApiPrefix}getpredictions?key=${ApiKey}&rt=${route}&stpid=${stopID}${format}`)
     .then(res => res.data['bustime-response'])
-    .then(predictions => {
+    .then(predictions =>
       // const arrivals = pred
-      return predictions;
-    })
-    .catch(err => console.error(err))
-
+      predictions
+    )
+    .catch(err => console.error(err));
 }
 
 String.prototype.replaceFunc = function () {
@@ -126,8 +122,8 @@ String.prototype.replaceFunc = function () {
     .replaceAll('(', '')
     .replaceAll(')', '')
     .replaceAll('-', ' ')
-    .replaceAll('.', '')
-}
+    .replaceAll('.', '');
+};
 
 String.prototype.replaceAll = function (search, replacement) {
   const target = this;
@@ -142,30 +138,42 @@ function getStopsFromRouteFile(routes) {
         const name = routes[route][direction][stopID];
         const stopName = routes[route][direction][stopID].replaceFunc();
         stopObj[stopID] = stopName;
-      })
-    })
-  })
-  writeToFile(stopObj, 'data/stops.json')
+      });
+    });
+  });
+  writeToFile(stopObj, 'data/stops.json');
 }
 
-getStopsFromRouteFile(allRoutes);
-
+// getStopsFromRouteFile(allRoutes);
+function checkWord(word) {
+  const badWords = {
+    street: 1,
+    avenue: 1,
+    avenuenue: 1,
+    boulevard: 1,
+    drive: 1
+  };
+  if (badWords[word]) {
+    return false;
+  }
+  return true;
+}
 
 function mapKeywords(allRoutes) {
   const routesWithKeywords = {};
   const memo = {};
   const keywordValues = [];
-  for (let route in allRoutes) {
+  for (const route in allRoutes) {
     if (allRoutes.hasOwnProperty(route)) {
       routesWithKeywords[route] = {};
-      for (let direction in allRoutes[route]) {
+      for (const direction in allRoutes[route]) {
         if (allRoutes[route].hasOwnProperty(direction)) {
           routesWithKeywords[route][direction] = {};
-          for (let stopId in allRoutes[route][direction]) {
+          for (const stopId in allRoutes[route][direction]) {
             if (allRoutes[route][direction].hasOwnProperty(stopId)) {
-              console.log(allRoutes[route][direction][stopId])
-              let stopName = allRoutes[route][direction][stopId];
-              let keywords = stopName
+              console.log(allRoutes[route][direction][stopId]);
+              const stopName = allRoutes[route][direction][stopId];
+              const keywords = stopName
                 .replaceAll('&', '')
                 .replaceAll('& ', '')
                 .replaceAll('/', ' ')
@@ -205,15 +213,17 @@ function mapKeywords(allRoutes) {
                 .split(' ')
                 .filter(word => word !== '');
               keywords.forEach(word => {
-                if (routesWithKeywords[route][direction][word]) {
-                  routesWithKeywords[route][direction][word].push(stopId);
-                } else {
-                  routesWithKeywords[route][direction][word] = [stopId]
+                if (checkWord(word.toLowerCase())) {
+                  if (routesWithKeywords[route][direction][word]) {
+                    routesWithKeywords[route][direction][word].push(stopId);
+                  } else {
+                    routesWithKeywords[route][direction][word] = [stopId];
+                  }
                 }
-              })
+              });
             }
           }
-          for (let keyword in routesWithKeywords[route][direction]) {
+          for (const keyword in routesWithKeywords[route][direction]) {
             if (routesWithKeywords[route][direction].hasOwnProperty(keyword)) {
               if (routesWithKeywords[route][direction][keyword].length > 20) {
                 delete routesWithKeywords[route][direction][keyword];
@@ -223,7 +233,7 @@ function mapKeywords(allRoutes) {
                 keywordValues.push({
                   value: keyword,
                   synonyms: [keyword]
-                })
+                });
               }
             }
           }
@@ -231,12 +241,11 @@ function mapKeywords(allRoutes) {
       }
     }
   }
-  console.log('$$$$$$', keywordValues.length)
-  writeToFile(routesWithKeywords, 'routesWithKeywords.json')
-  writeToFile(keywordValues, 'keywordValues.json')
-  writeToFileCsv(keywordValues, 'keyValues.csv')
+  console.log('$$$$$$', keywordValues.length);
+  writeToFile(routesWithKeywords, 'routesWithKeywords.json');
+  writeToFile(keywordValues, 'keywordValues.json');
+  writeToFileCsv(keywordValues, 'keyValues.csv');
 }
-
 
 
 module.exports = {
@@ -247,4 +256,4 @@ module.exports = {
   getStops,
   mapKeywords,
   getArrivals
-}
+};
--- index.js
+++ index.js
@@ -2,31 +2,23 @@ const routeMap = require('./data/routesWithKeywords');
 const stopMap = require('./data/stops');
 const helpers = require('./helpers');
 
-exports.getArrivals = function getArrivals(req, res) {
-  const response = "This is a sample response from your webhook!"
-  const { route, stops, direction } = req.body.result.parameters;
-  const stopID = getStopID(route, stops, direction);
-  const stopName = stopMap[stopID];
-  helpers.getArrivals(stopID, route)
-    .then(arrivals => {
-      const response = formatText(arrivals, route, stopName)
-      res.setHeader('Content-Type', 'application/json');
-      res.send(JSON.stringify({
-        "speech": response, "displayText": response
-      }));
-    })
+const data1 = {
+  route: 50,
+  direction: 'Northbound',
+  stops: ['Dickens']
 };
 
 function getStopID(route, stops, direction) {
   const mapObj = {};
   let count = 0;
   let stopIdToReturn;
-  stops.forEach(stop => {
-    let stopArr = routeMap[route][direction][stop]
+  for (let i = 0; i < stops.length; i++) {
+    const stopArr = routeMap[route][direction][stops[i]];
     if (stopArr) {
-      stopArr.forEach(stopID => {
+      for (let j = 0; j < stopArr.length; j++) {
+        const stopID = stopArr[j];
         if (mapObj[stopID]) {
-          mapObj[stopID]++;
+          mapObj[stopID] += 1;
         } else {
           mapObj[stopID] = 1;
         }
@@ -34,15 +26,17 @@ function getStopID(route, stops, direction) {
           stopIdToReturn = stopID;
           count = mapObj[stopID];
         }
-      })
+      }
     }
-  })
+  }
   return stopIdToReturn;
 }
 
+console.log(getStopID(data1.route, data1.stops, data1.direction));
+
 function formatText(arrivals, route, stopName) {
   let response;
-  const formatWithMinutes = function (arrival) {
+  function formatWithMinutes(arrival) {
     return arrival === '1' ? `${arrival} minute` : `${arrival} minutes`;
   }
   if (arrivals.error) {
@@ -51,51 +45,62 @@ function formatText(arrivals, route, stopName) {
       `there are no upcoming arrivals for route ${route}`
       :
       `there is no scheduled service for route ${route}`;
-  } else {
-    const due = arrivals.prd[0].prdctdn.toLowerCase() === 'due';
-    if (due) {
-      response = `hurry! a bus is due to arrive any second at ${stopName}. `
-    }
-    switch (arrivals.prd.length) {
+  }
+  const due = arrivals.prd[0].prdctdn.toLowerCase() === 'due';
+  if (due) {
+    response = `hurry! a bus is due to arrive any second at ${stopName}. `;
+  }
+  switch (arrivals.prd.length) {
+    case 1:
+      if (!due) {
+        response = `a bus is coming in ${formatWithMinutes(arrivals.prd[0].prdctdn)} to ${stopName}. `;
+      }
+      response += 'it\'s the last one coming for a while';
+      return response;
 
-      case 1:
-        if (!due) {
-          response = `a bus is coming in ${formatWithMinutes(arrivals.prd[0].prdctdn)} to ${stopName}. `
-        }
-        response += `it's the last one coming for a while`;
-        return response;
+    case 2:
+      if (!due) {
+        response = `buses are arriving in ${formatWithMinutes(arrivals.prd[0].prdctdn)} and ${formatWithMinutes(arrivals.prd[1].prdctdn)} to ${stopName}.`;
+      } else {
+        response += `the next one is arriving in ${formatWithMinutes(arrivals.prd[1].prdctdn)} to ${stopName}.`;
+      }
+      return response;
 
-      case 2:
-        if (!due) {
-          response = `buses are arriving in ${formatWithMinutes(arrivals.prd[0].prdctdn)} and ${formatWithMinutes(arrivals.prd[1].prdctdn)} to ${stopName}.`
-        } else {
-          response += `the next one is arriving in ${formatWithMinutes(arrivals.prd[1].prdctdn)} to ${stopName}.`;
-        }
-
-      default:
-        if (!due) {
-          const minuteSeq = arrivals.prd.map((prediction, i) => {
-            if (i === arrivals.prd.length - 1) {
-              return 'and ' + formatWithMinutes(prediction.prdctdn);
-            }
-            return prediction.prdctdn;
-          }).join(', ');
-          response = `buses are arriving in ${minuteSeq} to ${stopName}`
-        } else {
-          for (let i = 1; i < arrivals.prd.length; i++) {
-            if (arrivals.prd[i].prdctdn.toLowerCase() === 'due') {
-              response += 'the next one is due too';
-            } else {
-
-              if (i !== arrivals.prd.length - 1) {
-                response += arrivals.prd[i].prdctdn + ', ';
-              } else {
-                response += `and ${formatWithMinutes(arrivals.prd[i].prdctdn)} to ${stopName}.`
-              }
-            }
+    default:
+      if (!due) {
+        const minuteSeq = arrivals.prd.map((prediction, i) => {
+          if (i === arrivals.prd.length - 1) {
+            return `and ${formatWithMinutes(prediction.prdctdn)}`;
+          }
+          return prediction.prdctdn;
+        }).join(', ');
+        response = `buses are arriving in ${minuteSeq} to ${stopName}`;
+      } else {
+        for (let i = 1; i < arrivals.prd.length; i++) {
+          if (arrivals.prd[i].prdctdn.toLowerCase() === 'due') {
+            response += 'the next one is due too';
+          } else if (i !== arrivals.prd.length - 1) {
+            response += `${arrivals.prd[i].prdctdn}, `;
+          } else {
+            response += `and ${formatWithMinutes(arrivals.prd[i].prdctdn)} to ${stopName}.`;
           }
         }
-        return response
-    }
+      }
+      return response;
   }
 }
+
+exports.getArrivals = function getArrivals(req, res) {
+  // const response = 'This is a sample response from your webhook!';
+  const { route, stops, direction } = req.body.result.parameters;
+  const stopID = getStopID(route, stops, direction);
+  const stopName = stopMap[stopID];
+  helpers.getArrivals(stopID, route)
+    .then(arrivals => {
+      const response = formatText(arrivals, route, stopName);
+      res.setHeader('Content-Type', 'application/json');
+      res.send(JSON.stringify({
+        speech: response, displayText: response
+      }));
+    });
+};
